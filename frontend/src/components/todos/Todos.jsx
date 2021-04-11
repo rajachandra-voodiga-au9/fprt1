9cbf421594141e735a92572c331b50f1c4a93a74
rajachandra-voodiga-au9 <45597536+rajachandra-voodiga-au9@users.noreply.github.com>
2021-04-12 05:02:12 +0530

    Add files via upload

---
+++ frontend/src/components/todos/Todos.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import AddTodo from "./AddTodo";
+import ListTodos from "./ListTodos";
+
+import { useSelector } from "react-redux";
+
+const Todos = () => {
+  const auth = useSelector((state) => state.auth);
+  const [todo, setTodo] = useState({
+    name: "",
+    isComplete: false,
+  });
+
+  return (
+    <>
+      {auth._id ? (
+        <>
+          <ListTodos todo={todo} setTodo={setTodo} />
+        </>
+      ) : (
+        <>
+          <ListTodos todo={todo} setTodo={setTodo} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Todos;
